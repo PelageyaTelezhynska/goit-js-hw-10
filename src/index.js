f87daa9ff6b57b378132ab12b032d5099e65b1aa
PelageyaTelezhynska <p.yu.telezhynska@gmail.com>
2022-10-25 22:54:48 +0300

    add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import Notiflix from 'notiflix';
+import markupList from './templates/markupList.hbs';
+// import markupCard from './templates/markupCard.hbs';
+import { fetchCountries } from './fetchCountries';
 import './css/styles.css';
 const debounce = require('lodash.debounce');
 
@@ -6,10 +10,51 @@ const DEBOUNCE_DELAY = 300;
 const refs = {
   input: document.querySelector('#search-box'),
   list: document.querySelector('.country-list'),
+  card: document.querySelector('.country-info'),
 };
 
-refs.input.addEventListener('input', debounce(onInput, 300));
+refs.input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
 function onInput(e) {
-  return e.target.value.trim();
+  resetResult();
+  const formData = e.target.value.trim();
+
+  fetchCountries(formData)
+    .then(data => {
+      if (data.length > 10) {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        return;
+      }
+      if (data.length >= 2) {
+        refs.list.innerHTML = markupList(data);
+      } else {
+        refs.card.innerHTML = markupCard(data);
+      }
+    })
+    .catch(err =>
+      Notiflix.Notify.failure('Oops, there is no country with that name')
+    );
+}
+
+function resetResult() {
+  refs.list.innerHTML = '';
+  refs.card.innerHTML = '';
+}
+
+function markupCard(data) {
+  return data
+    .map(
+      item => `<div class = "wrapper"><img src='${
+        item.flags.svg
+      }' alt='flag of ${name.official}' />
+  <h2 class='country-name card'>${item.name.official}</h2></div>
+  <p><span>Capital:</span> ${item.capital}</p>
+  <p><span>Population:</span> ${item.population}</p>
+  <p><span>Languages:</span>
+   ${Object.values(item.languages).join(', ')}
+  </p>`
+    )
+    .join('');
 }
